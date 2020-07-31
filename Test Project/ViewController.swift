56c5f87d6ad7e42da334697f07ef414b75f31302
maedeh <maedeh@maedehs-mac.local>
2020-07-31 15:29:07 +0430

    Initial Commit

---
+++ Test Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Project
+//
+//  Created by maedeh on 5/10/1399 AP.
+//  Copyright © 1399 maedeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
